fcd9275f28f6a04f7e55eab3f3be0c7d0d64b248
Rong Yu <rong@RongMacBook-Air.local>
2023-10-05 18:43:06 +0200

    Feature: data

---
+++ content/project.tsx
@@ -0,0 +1,13 @@
+type project = {
+	id: string;
+	name: string;
+	date: string;
+	missions: Array<string>;
+	skill: Array<string>;
+	company?: string;
+	description: string;
+	link?: string;
+	github?: string;
+	imgUrls: Array<string>;
+	imageFolder: string;
+};
