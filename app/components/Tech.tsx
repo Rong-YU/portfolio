d6e1439eb9352a3aeec667fbb0700e2db380622b
Rong Yu <rong@RongMacBook-Air.local>
2023-10-08 16:56:09 +0200

    Feature: work page

---
+++ app/components/Tech.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React from "react";
+
+type Props = {
+	name: string;
+	numberOfRelatedProject: Number;
+};
+
+export default function Tech({ name, numberOfRelatedProject }: Props) {
+	return (
+		<motion.div
+			whileHover={{ scale: 1.1 }}
+			whileTap={{ scale: 1.1 }}
+			whileFocus={{ scale: 1.1 }}
+			className="relative hover:cursor-pointer"
+		>
+			<div>
+				{name} x{numberOfRelatedProject.toString()}
+			</div>
+		</motion.div>
+	);
+}
